cd3b5949b29993b8599d392759d925fb137dca0a
Elmer <infcle@gmail.com>
2018-02-06 11:15:55 -0400

    modelo seccion

---
+++ app/models/seccion/registro_model.php
@@ -0,0 +1,17 @@
+<?php 
+	require_once ("../../config/db.php");
+	require_once ("../../config/conexion.php");
+	require_once ("../../config/route.php");
+
+	//echo "<pre>";print_r ($_REQUEST);echo "</pre>";
+	$nombre = trim($_POST["nombre"]);
+	$limite = trim($_POST["limite"]);
+
+	$sql = "INSERT INTO seccion(nombre, limite) VALUES('{$nombre}', {$limite})";
+
+	if (!$con->query($sql)) {
+		echo "Falló la insercion: (" . $con->errno . ") " . $con->error;
+	}
+	else
+		echo 1;
+?>

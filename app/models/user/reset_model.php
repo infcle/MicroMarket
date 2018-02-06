830da49a49bb04ffdc5bb3875735eee0cd602876
Elmer <infcle@gmail.com>
2018-02-06 19:30:59 -0400

    reset password

--- app/models/user/reset_model.php
+++ app/models/user/reset_model.php
@@ -2,6 +2,12 @@
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 	require_once ("../../config/route.php");
-
-	$sql = "UPDATE usuario_login SET contrsenia";
+	$user_password = 123123;
+	$user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
+	$sql = "UPDATE usuario_login SET contrasenia='{$user_password_hash}'";
+	if (!$con->query($sql)) {
+		echo 'Falló : (' . $con->errno . ') ' . $con->error;
+	}
+	else
+		echo 1;
 ?>

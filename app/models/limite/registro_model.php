bd369ffed97575e665ab38496699f674f50bd5f9
Elmer <infcle@gmail.com>
2018-02-08 15:43:06 -0400

    cambios en modelo de limites

--- app/models/limite/registro_model.php
+++ app/models/limite/registro_model.php
@@ -2,10 +2,8 @@
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 
-	//echo "<pre>";print_r ($_REQUEST);echo "</pre>";
 	$nombre = trim($_POST["nombre"]);
 	$limite = trim($_POST["limite"]);
-	$subsidio=trim($_POST["subsidio"]);
 	$fechaRegistro = date('Y-m-d H:i:s');
 
 	$sql = "INSERT INTO limite(nombre, limite,fecha_registro) VALUES('{$nombre}', {$limite},'{$fechaRegistro}')";

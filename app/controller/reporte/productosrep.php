8dc7d769deabdbd65c11e244939ebe426437e32a
laiwett <l.mendoza.liet@gmail.com>
2018-02-16 22:10:02 -0400

    Reporte16_3

--- app/controller/reporte/productosrep.php
+++ app/controller/reporte/productosrep.php
@@ -6,7 +6,7 @@ require_once('../../librarys/tcpdf/examples/tcpdf_include.php');
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// set document information
+// set document information 
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Nicola Asuni');
 $pdf->SetTitle('TCPDF Example 001');
@@ -128,13 +128,18 @@ if (!empty($hidetop)){
         $pdf->MultiCell($posxnom-$posxnro-1, 1,$fila['nombreuno'] , 0, 'L',0);
 
         $pdf->SetXY($posxnom, $curY);
-        $pdf->MultiCell($posxtip-$posxnom-1, 1,$fila['nombredos'] , 0, 'C',0);
+        if($fila['nro_plu'] == 1){
+            $pdf->MultiCell($posxtip-$posxnom-1, 1,"Cantidad", 0, 'C',0);
+        }
+        if($fila['nro_plu'] == 2){
+            $pdf->MultiCell($posxtip-$posxnom-1, 1,"Peso" , 0, 'C',0);
+        }    
 
         $pdf->SetXY($posxtip, $curY);
-        $pdf->MultiCell($posxpre-$posxtip-1, 1,$fila['nro_plu'] , 0, 'C',0);
+        $pdf->MultiCell($posxpre-$posxtip-1, 1,$fila['precio'] , 0, 'C',0);
 
         $pdf->SetXY($posxpre, $curY);
-        $pdf->MultiCell($posxplu-$posxpre-1, 1,$fila['nombreuno'] , 0, 'C',0);
+        $pdf->MultiCell($posxplu-$posxpre-1, 1,$fila['cod_plu'] , 0, 'C',0);
 
         $pdf->SetXY($posxplu, $curY);
         $pdf->MultiCell($posxfin-$posxplu-1, 1,$fila['nombredos'] , 0, 'L',0);

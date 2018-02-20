8cd9e7dce800a2dfe6171f78a577e7f683afa7b8
Elmer Coronel <infcle@gmail.com>
2018-02-20 09:33:28 -0400

    cambio spequeños

--- public/views/producto/modal_editar.php
+++ public/views/producto/modal_editar.php
@@ -7,6 +7,10 @@
             </div>
             <div class="modal-body">
                 <form role="form" id="frmEditar" name="frmEditar">
+                    <div class=" modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary" id="btnEditar" id="btnEditar">Editar</button>
+                    </div>
                 </form>
             </div>
         </div>

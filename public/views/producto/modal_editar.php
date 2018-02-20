2eba65f53e73fc5a8bb950b376a8a3a78d3563e9
sergui <juansergio1981@gmail.com>
2018-02-20 16:46:41 -0400

    modificacion producto

--- public/views/producto/modal_editar.php
+++ public/views/producto/modal_editar.php
@@ -10,7 +10,7 @@
                     <input type="hidden" name="id_producto" id="id_producto" class="form-control" value="">
                     <div class="form-group">
                         <label for="Nombre">Numero de PLU</label>
-                        <input class=" form-control" id="plu" name="plu" type="text" required autofocus="true" disabled="true" />
+                        <input class=" form-control" id="plu" name="plu" type="text" required autofocus="true" readonly="true" />
                     </div>
                     <div class="form-group">
                         <label for="Nombre">Nuevo Nombre del producto</label>

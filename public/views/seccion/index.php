6320074aba8ddb4aff1e7430c39660a110c4b48b
Elmer <infcle@gmail.com>
2018-02-19 20:39:38 -0400

    coreegido

--- public/views/seccion/index.php
+++ public/views/seccion/index.php
@@ -4,11 +4,11 @@
             <header class="panel-heading">
                 <div class="row panel-heading">
                     Lista de secciones
-                  <span class="pull-right">
-                    <a href="#modal_Registrar" class="btn btn-xs btn-success" data-toggle="modal">
-                        <span class="fa fa-pencil"></span> Nueva seccion
-                    </a>
-                 </span>
+                    <span class="pull-right">
+                        <a href="#modal_Registrar" class="btn btn-xs btn-success" data-toggle="modal">
+                            <span class="fa fa-pencil"></span> Nueva seccion
+                        </a>
+                    </span>
                 </div>
             </header>
             <div class="panel-body">
@@ -27,110 +27,95 @@
                                     <td><?php echo $seccion['id_seccion']; ?></td>
                                     <td><?php echo $seccion['nombre']; ?></td>
                                     <td >
-                                         <a class="btn btn-success" href="#modalEditar" role="button" data-placement="top" title="Editar" data-toggle="modal" onclick="obtener_datos(<?php echo $seccion['id_seccion'] ?>)">
-                                            <span class="fa fa-edit" ></span>
-                                        </a>
-                                        <a class="btn btn-danger" href="#modalEliminar" role="button" data-toggle="modal" data-placement="top" title="Eliminar" onclick="eliminar_datos(<?php echo $seccion['id_seccion'] ?>)">
-                                            <span class="fa fa-trash-o"></span>
-                                        </a>
-                                    </td>
-                                </tr>
-                            <?php endforeach;?>
-                        </tbody>
-                    </table>
-                </div>
+                                       <a class="btn btn-success" href="#modalEditar" role="button" data-placement="top" title="Editar" data-toggle="modal" onclick="obtener_datos(<?php echo $seccion['id_seccion'] ?>)">
+                                        <span class="fa fa-edit" ></span>
+                                    </a>
+                                    <a class="btn btn-danger" href="#modalEliminar" role="button" data-toggle="modal" data-placement="top" title="Eliminar" onclick="eliminar_datos(<?php echo $seccion['id_seccion'] ?>)">
+                                        <span class="fa fa-trash-o"></span>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                </table>
             </div>
-            <?php require_once 'modal_registrar.php'; ?>
-            <?php require_once 'modal_eliminar.php'; ?>
-             <?php require_once 'modal_editar.php'; ?>
+        </div>
+        <?php require_once 'modal_registrar.php'; ?>
+        <?php require_once 'modal_eliminar.php'; ?>
+        <?php require_once 'modal_editar.php'; ?>
 
-        </section>
-    </div>
+    </section>
+</div>
 </div>
 <script>
-     function obtener_datos(id){
+   function obtener_datos(id){
+    $.ajax({
+        url: '../../models/seccion/datos_seccion.php',
+        type: 'POST',
+        dataType: "json",
+        data: {id_seccion: id},
+        success: function(datos){
+            $("#name").val(datos['seccion']['nombre']);
+            $("#id_seccion_modificar").val(datos['seccion']['id_seccion']);
+        }
+    });
+}
+function eliminar_datos(id){
+    $("#id_eliminar").val(id);
+}
+
+$(document).ready(function() {
+    $("#btnEliminar").click(function(event) {
         $.ajax({
-            url: '../../models/seccion/datos_seccion.php',
+            url: '../../models/seccion/eliminar_model.php',
             type: 'POST',
-            dataType: "json",
-            data: {id_seccion: id},
+            data: $("#frmEliminar").serialize(),
             success: function(datos){
-                $("#name").val(datos['seccion']['nombre']);
-                $("#id_seccion_modificar").val(datos['seccion']['id_seccion']);
-                
-                
+                $('#modalEliminar').modal('hide');
+                $('#btnEliminar').attr({disabled: 'true'});
+                transicionSalir();
+                mensajes_alerta('DATOS ELIMINADOS ELIMINADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                setTimeout(function(){
+                    window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
+                }, 3000);
             }
         });
+    });
+    $('#frmEditar').validate({
+        debug:true,
+        rules:{
+            name:{
+                required:true,
+                minlength: 4,
+            },
 
-          
-    }
-    function eliminar_datos(id){
-                $("#id_eliminar").val(id);
-    }
-
-       $(document).ready(function() {
-             $("#btnEliminar").click(function(event) {
+        },
+        submitHandler: function (form) {
             $.ajax({
-                url: '../../models/seccion/eliminar_model.php',
-                type: 'POST',
-                data: $("#frmEliminar").serialize(),
-                success: function(datos){
-                    ('#modalEliminar').modal('hide');
-                    $('#btnEliminar').attr({
-                        disabled: 'true'
-                    });
-                    transicionSalir();
-                    mensajes_alerta('DATOS ELIMINADOS ELIMINADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
-                    setTimeout(function(){
+                url: '../../models/seccion/editar_model.php',
+                type: 'post',
+                data: $("#frmEditar").serialize(),
+                beforeSend: function() {
+                    transicion("Procesando Espere....");
+                },
+                success: function(response) {
+                    if(response==1){
+                        $('#modalEditar').modal('hide');
+                        $('#btnEditar').attr({
+                            disabled: 'true'
+                        });
+                        transicionSalir();
+                        mensajes_alerta('DATOS EDITADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                        setTimeout(function(){
                             window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
                         }, 3000);
+                    }else{
+                        transicionSalir();
+                        mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
                     }
-                });
-             });
-
-            $('#frmEditar').validate({
-            debug:true,
-            rules:{
-                name:{
-                    required:true,
-                    minlength: 4,
-                },
-              
-            },
-          
-            submitHandler: function (form) {
-                $.ajax({
-                    url: '../../models/seccion/editar_model.php',
-                    type: 'post',
-                    data: $("#frmEditar").serialize(),
-                    beforeSend: function() {
-                        transicion("Procesando Espere....");
-                    },
-                    success: function(response) {
-                        if(response==1){
-                            $('#modalEditar').modal('hide');
-                            $('#btnEditar').attr({
-                                disabled: 'true'
-                            });
-                            transicionSalir();
-                            mensajes_alerta('DATOS EDITADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
-                            setTimeout(function(){
-                                window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
-                            }, 3000);
-                        }else{
-                            transicionSalir();
-                            mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
-                        }
-                    }
-                });
-            }
-        });
-       });
-
-      
-
-
-    
-
-
+                }
+            });
+        }
+    });
+});
 </script>

529b7d185096b7ce88b5051577c5fde670639d90
Elmer <infcle@gmail.com>
2018-02-20 11:10:10 -0400

    comparacion

--- public/views/seccion/modal_registrar.php
+++ public/views/seccion/modal_registrar.php
@@ -7,13 +7,10 @@
             </div>
             <div class="modal-body">
                 <form role="form" id="frmRegistrar" name="frmRegistrar">
-                   
                     <div class="form-group>
                         <label for="Nombre">Nombre de Seccion</label>
                         <input class=" form-control" id="nombre" name="nombre" type="text" />
                     </div>
-                  
-                   
                     <div class=" modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
                         <button type="submit" class="btn btn-primary" id="btnRegistrar" >Crear</button>

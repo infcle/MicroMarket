0f94b45087dba5fe0c22b279e34555be33cbe87b
dodonet78 <maydanafroy@gmail.com>
2018-02-23 17:06:41 -0400

    hoy

--- scripts bd/bdsusi.sql
+++ scripts bd/bdsusi.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 10.1.8-MariaDB : Database - marketbd
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
